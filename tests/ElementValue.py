b139f9f972f48cee07a59c5b71e0d5d1f85e950d
trongdoan6075 <trongdoan6075@gmail.com>
2020-05-12 00:26:02 -0700

    add comment

--- tests/ElementValue.py
+++ tests/ElementValue.py
@@ -1,5 +1,9 @@
 from selenium.webdriver.common.by import By
 
+# This file contains the xpath value to any element from source page.
+# If xpath changes later in time, we will only need to change the value once
+# from this file.
+
 _str1 = (By.XPATH, "//div/div[2]")     # first lorem ipsum text 's element
 _str2 = (By.XPATH, "//div[2]/div[2]")  # first lorem ipsum text 's element
 _str3 = (By.XPATH, "//div[3]/div[2]")  # first lorem ipsum text 's element
